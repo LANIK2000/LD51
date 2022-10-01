98d208c5446f053e8067d541246499f137b2ccf8
Michal-Atlas <michal_atlas+repo@posteo.net>
2022-10-01 15:26:37 +0200

    Auto add LoopingEntities to LoopSystem
    also fixed typo

--- Assets/Scripts/LoopingEntity.cs
+++ Assets/Scripts/LoopingEntity.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LoopingEntity : MonoBehaviour
 {
 	private Vector2 _velocity = new Vector2();
-	private Vector3 _position = new Vector3();
+	private Vector2 _position = new Vector2();
 	public void Save() {
 		_velocity = _rb.velocity;
 		_position = _rb.position;
@@ -18,5 +18,6 @@ public class LoopingEntity : MonoBehaviour
 	protected Rigidbody2D _rb;
 	void Start() {
 		_rb = GetComponent<Rigidbody2D>();
+		LoopSaveSystem.instance.Entities.Add(this);
 	}
 }
