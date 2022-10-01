383df028b109bedfb7e23a4c80980dfe90609d87
LANIK2000 <lanikmail@gmail.com>
2022-10-01 21:25:19 +0200

    LoopingEntity now saves rotation

--- Assets/Scripts/LoopingEntity.cs
+++ Assets/Scripts/LoopingEntity.cs
@@ -6,13 +6,16 @@ public class LoopingEntity : MonoBehaviour
 {
 	private Vector2 _velocity = new Vector2();
 	private Vector2 _position = new Vector2();
+	private float _rotation = 0;
 	public virtual void Save() {
 		_velocity = _rb.velocity;
 		_position = _rb.position;
+		_rotation = _rb.rotation;
 	}
 	public virtual void Load() {
 		_rb.velocity = _velocity;
 		_rb.position = _position;
+		_rb.rotation = _rotation;
 	}
 
 	protected Rigidbody2D _rb;
