fc93699323273eeee74a923d6ef61222e54850a8
LANIK2000 <lanikmail@gmail.com>
2022-10-02 20:30:44 +0200

    Sneck fix and better Player attack

--- Assets/Prefabs/Entities/Snake/Snek.cs
+++ Assets/Prefabs/Entities/Snake/Snek.cs
@@ -2,52 +2,87 @@ using System;
 using UnityEngine;
 using Random = Unity.Mathematics.Random;
 
-public class Snek : MonoBehaviour
+public class Snek : LoopingEntity
 {
-    public float MoveForce = 6;
-    public float MoveFrequency = 1f;
-    public int RotationFrequency = 50;
-    private bool FacingRight = false;
+	public float MoveForce = 6;
+	public float MoveFrequency = 1f;
+	private bool FacingRight = false;
+	bool _savedFlip = false;
+	float Timer = 0;
+	float _savedTime = 0;
 
-    public void seesPit(Collider2D cl)
-    {
-        if (cl.tag != "Player")
-        {
-            _rb.velocity = Vector2.zero;
-            Rotate();
-        }
-    }
+	public override void Save() {
+		base.Save();
+		_saved_alive = _alive;
+		_savedFlip = FacingRight;
+		_savedTime = Timer;
+	}
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _rb = GetComponent<Rigidbody2D>();
-    }
+	public override void Load() {
+		base.Load();
+		_alive = _saved_alive;
+		gameObject.SetActive(_saved_alive);
+		FacingRight = _savedFlip;
+		_spriteRenderer.flipX = FacingRight;
+		Timer = _savedTime;
+	}
 
-    float Timer { get; set; } = 0;
+	bool _saved_alive = true;
+	bool _alive = true;
+	void Die(){
+		_alive = false;
+		gameObject.SetActive(false);
+	}
 
-    private Rigidbody2D _rb;
-    private SpriteRenderer _spriteRenderer;
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "PlayerAttack")
+			this.Die();
+		if (other.tag == "Player")
+			LoopSaveSystem.instance.LoadAll();
+	}
 
-    private static Random _rand = Random.CreateFromIndex(0);
+	public void seesPit(Collider2D cl)
+	{
+		if (cl.tag == "Ground")
+		{
+			_rb.velocity = Vector2.zero;
+			Rotate();
+		}
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        Timer -= Time.deltaTime;
-        if (Timer <= 0)
-        {
-            _rb.velocity += new Vector2( MoveForce * (FacingRight ? 1 : -1), 0);
-            Timer = MoveFrequency;
-            if (_rand.NextInt(RotationFrequency) == 0)
-                Rotate();
-        }
-    }
+	public void seesWall(Collider2D cl)
+	{
+		if (cl.tag == "Ground")
+		{
+			_rb.velocity = Vector2.zero;
+			Rotate();
+		}
+	}
 
-    void Rotate()
-    {
-        FacingRight = !FacingRight;
-        _spriteRenderer.flipX = FacingRight;
-    }
+	// Start is called before the first frame update
+	protected override void Start()
+	{
+		base.Start();
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+		_rb = GetComponent<Rigidbody2D>();
+	}
+
+	private SpriteRenderer _spriteRenderer;
+
+	// Update is called once per frame
+	void Update()
+	{
+		Timer -= Time.deltaTime;
+		if (Timer <= 0)
+		{
+			_rb.velocity = new Vector2( MoveForce * (FacingRight ? 1 : -1), 0);
+			Timer = MoveFrequency;
+		}
+	}
+
+	void Rotate()
+	{
+		FacingRight = !FacingRight;
+		_spriteRenderer.flipX = FacingRight;
+	}
 }
